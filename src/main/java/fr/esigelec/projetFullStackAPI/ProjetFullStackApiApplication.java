e9645055193a59291d8bdb04edbea2c6c4a070b0
Masterkoki <nonotankojason@gmail.com>
2025-11-17 10:11:33 +0100

    1er commit

---
+++ src/main/java/fr/esigelec/projetFullStackAPI/ProjetFullStackApiApplication.java
@@ -0,0 +1,13 @@
+package fr.esigelec.projetFullStackAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFullStackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFullStackApiApplication.class, args);
+	}
+
+}
