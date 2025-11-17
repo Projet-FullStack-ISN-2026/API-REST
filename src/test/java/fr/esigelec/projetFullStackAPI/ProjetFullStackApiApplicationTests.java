7bdddcac8863681b6cf40328673e1f61ec306af1
Masterkoki <94070984+Masterkoki@users.noreply.github.com>
2025-11-17 11:05:50 +0100

    Squelette API

---
+++ src/test/java/fr/esigelec/projetFullStackAPI/ProjetFullStackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esigelec.projetFullStackAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFullStackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
