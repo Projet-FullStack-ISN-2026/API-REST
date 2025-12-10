10354cf5c3e1eb6e3601f748ddf8e0a65d4fdb22
Maizze <matthieu.tevane@groupe-esigelec.org>
2025-12-10 10:09:31 +0100

    Supression des fichier dto, entity, repo

--- src/main/java/com/tf8/quizapp/repository/QuestionsRepository.java
+++
@@ -1,5 +0,0 @@
-package com.tf8.quizapp.repository;
-
-public interface QuestionsRepository {
-
-}
--- src/main/java/com/tf8/quizapp/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tf8.quizapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tf8.quizapp.model.entity.QuizEntity;
-
-public interface QuizRepository extends JpaRepository<QuizEntity, Long> {
-
-}
