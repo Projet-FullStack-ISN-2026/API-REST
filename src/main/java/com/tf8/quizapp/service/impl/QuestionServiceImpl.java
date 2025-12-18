eec07dd6a1ef045e2425f57c14fc3accc9df4a0c
Masterkoki <94070984+Masterkoki@users.noreply.github.com>
2025-12-18 11:33:00 +0100

    implémentation /admin/answer (#12)
    * implémentation /admin/answer
    
    * mise en Integer du status qiz
    
    * fix: isCorrect getter & methods organization
    
    ---------
    
    Co-authored-by: Art333 <rocknnarthur@gmail.com>

--- src/main/java/com/tf8/quizapp/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/tf8/quizapp/service/impl/QuestionServiceImpl.java
@@ -93,7 +93,7 @@ public class QuestionServiceImpl implements QuestionService {
                 OptionsDTO optDto = new OptionsDTO();
                 optDto.setId(optEntity.getId());
                 optDto.setText(optEntity.getText());
-                optDto.setIsCorrect(optEntity.getIsCorrect());
+                optDto.setIsCorrect(optEntity.isCorrect());
                 return optDto;
             }).collect(Collectors.toList());
             dto.setOptions(optionsDtos);
